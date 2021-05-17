107da57c6c02c63811ff38cdde1659606c895dbb
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2021-05-17 11:18:24 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -15,21 +15,25 @@ class App extends Component {
       pics: [
         
       ]
-    };
+    }
   }
 
 componentDidMount() {
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=dogs&per_page=24&format=json&nojsoncallback=1`).then(response => {
-    this.setState({pics: response.data.photos.photo})
-    })}
+    this.performSearch("art");
+}
 
 
 performSearch(query) {
+  this.startOver();
   axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=${query}&per_page=24&format=json&nojsoncallback=1`).then(response => {
     this.setState({pics: response.data.photos.photo});
   }).catch(
      error => console.log(error)
    );
+}
+
+startOver() {
+  this.setState({pics: []});
 }
 
 render() {
@@ -41,10 +45,10 @@ render() {
           <Switch>
             <Route exact path="/" component={() => <Redirect to = "./Home"/>} />
             <Route path="/Home" component={() => <Home data={this.state.pics} onSearch={this.performSearch} />} />
-            <Route path="/Cats" component={Home} />
-            <Route path="/Dogs" component={Home} />
-            <Route path="/Computers" component={Home} />
-            <Route path="/Results" component={() => <Results data={this.state.pics}/> } />
+            <Route path="/Cats" component={() => <Results data={this.state.pics} onSearch={this.performSearch("cats")} />} />
+            <Route path="/Dogs" component={() => <Results data={this.state.pics} onSearch={this.performSearch("dogs")} />} />
+            <Route path="/Computers" component={() => <Results data={this.state.pics} onSearch={this.performSearch("computers")} />} />
+            <Route path="/Results" component={() => <Results data={this.state.pics} onSearch={this.performSearch}/> } />
             <Route component={NotFound} />
           </Switch>
         </div>
