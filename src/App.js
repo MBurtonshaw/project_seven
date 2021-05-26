e457d4766b4c85b03eb672630015ce9c14946c82
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2021-05-26 10:04:22 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './css/App.css';
 import React, { Component } from "react";
 import NotFound from "./components/NotFound";
-import Home from "./components/Home";
+import Results from "./components/Results";
 import api from "./components/Config";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import axios from "axios";
 import SearchForm from "./components/SearchForm";
-import Cats from "./components/Cats";
-import Dogs from "./components/Dogs";
-import Birds from "./components/Birds";
-import Search from "./components/Search";
 
 class App extends Component {
   constructor() {
@@ -18,24 +14,19 @@ class App extends Component {
       pics: [
         
       ],
-      header: ""
+      query: ""
     }
   }
 
-componentDidMount() {
+  componentDidMount() {
     this.performSearch("nature");
-}
-
-clearState = () => {
-  this.setState({ header: "" });
-  this.setState({ pics: "" });
-}
+  }
 
 performSearch(query) {
   axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${ api }&tags=${ query }&per_page=24&format=json&nojsoncallback=1`).then(response => {
-    if (response.data.photos.total > 0) {
+    if (response.data.photos) {
       this.setState({ pics: response.data.photos.photo });
-      this.setState({ header: query });
+      this.setState({ query: query });
     } else {
       return(<NotFound />)
     }
@@ -48,13 +39,9 @@ render() {
       <BrowserRouter>
         <div className="container">
         <h1>Picture Search</h1>
-        <SearchForm onSearch={ this.performSearch.bind(this) } />
+        <SearchForm onSearch={ this.performSearch.bind(this) } query={this.state.query} />
           <Switch>
-            <Route exact path="/" component={ () => <Home data={ this.state.pics } header={ this.state.header } /> } />
-            <Route path="/cats" component={ () => <Cats data={ this.state.pics } header={ this.state.header } /> } />
-            <Route path="/dogs" component={ () => <Dogs data={ this.state.pics } header={ this.state.header } /> } />
-            <Route path="/birds" component={ () => <Birds data={ this.state.pics } header={ this.state.header } /> } />
-            <Route path="/search" component={ () => < Search data={ this.state.pics } header={ this.state.header } /> } />
+            <Route path="/" component={ () => <Results onSearch={ this.performSearch.bind(this) } query={ this.state.query } data={this.state.pics} /> } />
             <Route component={ NotFound } />
           </Switch>
         </div>
