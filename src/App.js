2d21e87fd56d111d319fc278feff36d35fef8eff
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2021-05-27 18:44:05 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import React, { Component } from "react";
 import NotFound from "./components/NotFound";
 import Main from "./components/Main";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import hashHistory from "react";
 
 class App extends Component {
 
 render() {
     return (
-      <BrowserRouter history={hashHistory}>
+      <BrowserRouter>
         <div className="container">
         <h1>Picture Search</h1>
+
           <Switch>
+            {/* Home route */}
             <Route exact path="/" component={ () => <Main /> } />
+
+            {/* Headers passed down as components to initiate a search when mounted */}
+            {/* Included for NavLink purposes in SearchForm.js */}
             <Route path="/cats" component={ () => <Main header={ "cats" } /> } />
             <Route path="/dogs" component={ () => <Main header={ "dogs" }/> } />
             <Route path="/birds" component={ () => <Main header={ "birds" }/> } />
             <Route path="/search/:term" component={ () => <Main /> } />
+
+            {/* 404 Error- in progress */}
             <Route component={ NotFound } />
           </Switch>
         </div>
