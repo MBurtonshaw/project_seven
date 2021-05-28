31b6c87733d797628e2c7def4d3add0177c4ee67
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2021-05-28 18:55:16 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,8 +3,32 @@ import React, { Component } from "react";
 import NotFound from "./components/NotFound";
 import Main from "./components/Main";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import api from "./components/Config";
+import axios from "axios";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pics: [
+        
+      ],
+      query: "",
+      //isLoading state - in progress
+      isLoading: false
+    }
+  }
+
+  performSearch = (query = "nature") => {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${ api }&tags=${ query }&per_page=24&format=json&nojsoncallback=1`).then(response => {
+          this.setState({ pics: response.data.photos.photo });
+          this.setState({ query: query });
+    })
+  }
+
+  componentDidMount() {
+    this.performSearch("nature");
+  }
 
 render() {
     return (
@@ -14,15 +38,14 @@ render() {
 
           <Switch>
             {/* Home route */}
-            <Route exact path="/" component={ () => <Main /> } />
+            <Route exact path="/" component={ () => <Main header={this.state.query}/> } />
 
-            {/* Headers passed down as components to initiate a search when mounted */}
-            {/* Included for NavLink purposes in SearchForm.js */}
+            {/* Headers passed down as props to initiate a search when Main.js is mounted */}
+            {/* Also for NavLink purposes in SearchForm.js */}
             <Route path="/cats" component={ () => <Main header={ "cats" } /> } />
             <Route path="/dogs" component={ () => <Main header={ "dogs" }/> } />
             <Route path="/birds" component={ () => <Main header={ "birds" }/> } />
             <Route path="/:term" component={ () => <Main /> } />
-
             {/* 404 Error- in progress */}
             <Route component={ NotFound } />
           </Switch>
