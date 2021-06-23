7518afa25699957572b0db3ce30208ea41e0c4c0
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2021-06-23 11:06:18 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import React, { Component } from "react";
 import NotFound from "./components/NotFound";
 import Main from "./components/Main";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import api from "./components/Config";
-import axios from "axios";
 import SearchForm from "./components/SearchForm";
-import Search from "./components/Search";
 
 class App extends Component {
   constructor() {
@@ -20,16 +17,17 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    this.performSearch_fromApp();
+  }
+
 //Search function sending an axios request to fetch data from flikr based on the search query
 //The resulting pictures and the query are saved to state
 performSearch_fromApp = (query) => {
         this.setState({ isLoading: true }, () => {     
-          axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${ api }&tags=${ query }&per_page=24&format=json&nojsoncallback=1`).then(response => {
         this.setState({ query: query });
-        this.setState({ pics: response.data.photos.photo });
         this.setState({ isLoading: false });
         })
-  })
 }
 
 render() {
@@ -38,7 +36,7 @@ render() {
         <div className="container">
         
         <h1>Picture Search</h1>
-        <SearchForm onSearch={ this.performSearch_fromApp} />
+          <SearchForm onSearch={this.performSearch_fromApp}/>
           <Switch>
             {/* Home route */}
             <Route exact path="/" component={ () => <Redirect to="/dogs" /> }/>
@@ -48,7 +46,7 @@ render() {
             <Route exact path="/cats" component={ () => <Main header={ "cats" } /> } />
             <Route exact path="/dogs" component={ () => <Main header={ "dogs" } /> } />
             <Route exact path="/birds" component={ () => <Main header={ "birds" } /> } />
-            <Route path="/:term" component={ () => <Search header={ this.state.query } data={ this.state.pics } isLoading={this.state.isLoading}/> } />
+            <Route path="/:term" component={ () => <Main header={this.state.query}/> } />
 
             {/* 404 Error- in progress */}
             <Route component={ NotFound } />
