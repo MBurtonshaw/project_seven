b24c3c33a956bb1f3842c7b6bf8e73d0dd8d785b
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2021-06-25 10:06:22 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import './css/App.css';
 import React, { Component } from "react";
-import NotFound from "./components/NotFound";
-import Main from "./components/Main";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import Main from "./components/Main";
 import SearchForm from "./components/SearchForm";
+import NotFound from "./components/NotFound";
+import './css/App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      query: "",
+      app_query: "",
       isLoading: false
     }
   }
 
   //Checking to see if there was a search executed without a url change
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.query !== this.state.query) {
-      this.messenger(this.state.query);
+  componentDidUpdate( prevProps, prevState ) {
+    if ( prevState.app_query !== this.state.app_query ) {
+      this.send_message_to_main( this.state.app_query );
     }
   }
 
 //Search function sending an axios request to fetch data from flikr based on the search query
 //The resulting pictures and the query are saved to state
-  messenger = (query) => {
+  send_message_to_main = ( query ) => {
         this.setState({ isLoading: true }, () => {     
-        this.setState({ query: query });
+        this.setState({ app_query: query });
         this.setState({ isLoading: false });
         })
 }
@@ -38,7 +38,7 @@ render() {
         <h1>Picture Search</h1>
 
           {/* Passing down function to SearchForm.js to retrieve the search query and then pass it to Main.js */}
-          <SearchForm onSearch={this.messenger}/>
+          <SearchForm onSearch={ this.send_message_to_main }/>
 
           <Switch>
             {/* Home route */}
@@ -51,7 +51,7 @@ render() {
             <Route exact path="/cats" component={ () => <Main header={ "cats" } /> } />
             <Route exact path="/dogs" component={ () => <Main header={ "dogs" } /> } />
             <Route exact path="/birds" component={ () => <Main header={ "birds" } /> } />
-            <Route path="/:term" component={ () => <Main header={this.state.query}/> } />
+            <Route path="/:term" component={ () => <Main header={ this.state.app_query }/> } />
 
             {/* 404 Error */}
             <Route component={ NotFound } />
